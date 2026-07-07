9a7c86d901798832f18f4311604898440c4a2fb7
ssuuppeerrmmaann <paulargueta@gmail.com>
2026-07-07 08:27:56 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 // FILE: main.tsx
-// TITLE: Main Application Entry Point
+// TITLE: Main Application Entry Point with Hash Routing
 
-// SECTION: Core Imports
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage.tsx';
+import IndianaPage from './IndianaPage.tsx';
 import './index.css';
 
-// SECTION: DOM Render
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePage />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/indiana" element={<IndianaPage />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 );
